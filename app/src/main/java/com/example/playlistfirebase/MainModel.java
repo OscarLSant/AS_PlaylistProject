ade76890168f40f7f4c80c7bee7ff9e1d7e6b6d1
OscarLS <oluna1103@gmail.com>
2023-11-14 10:46:00 -0600

    subida a github

---
+++ app/src/main/java/com/example/playlistfirebase/MainModel.java
@@ -0,0 +1,47 @@
+package com.example.playlistfirebase;
+
+public class MainModel {
+    String name, course, email, profile;
+
+    public MainModel() {
+    }
+
+    public MainModel(String name, String course, String email, String profile) {
+        this.name = name;
+        this.course = course;
+        this.email = email;
+        this.profile = profile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+}
